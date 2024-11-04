3c33f587128ddbb8d2de3c0e883a3fef01ea5494
DriesMelottePXL <75361303+DriesMelottePXL@users.noreply.github.com>
2024-11-04 21:35:27 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const cors = require('cors');
 const healthRoutes = require('./routes/health.routes');
 const todoRoutes = require('./routes/todo.routes');
 const carrouselRoutes = require('./routes/carrousel.routes');
@@ -9,8 +10,10 @@ const app = express();
 const db = require('./db');
 
 
+
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/health', healthRoutes);
