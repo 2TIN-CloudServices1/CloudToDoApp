f1214dd3803f024de2df5c5f8f7349eca0740acd
DriesMelottePXL <75361303+DriesMelottePXL@users.noreply.github.com>
2024-11-05 21:56:15 +0100

    Update carrousel.routes.js

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,40 +1,39 @@
 const express = require('express');
 const carrouselRouter = express.Router();
-
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: 'us-east-1',
-});
 
-carrouselRouter.get('', (req, res) => {
+const s3 = new AWS.S3();
+
+async function getImageUrlsFromS3Bucket() {
     const params = {
-        Bucket: process.env.BUCKET,
+        Bucket: '11904402-pe-bucket'
     };
 
-    s3.listObjectsV2(params, (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-
-        const jpgImages = data.Contents.filter((obj) => {
-            return obj.Key.endsWith('.jpg');
-        });
-
-        const images = jpgImages.map((obj) => {
-            const url = s3.getSignedUrl('getObject', {
-                Bucket: params.Bucket,
-                Key: obj.Key,
-                Expires: 60,
+    try {
+        console.log('Fetching images from S3 bucket...');
+        const data = await s3.listObjectsV2(params).promise();
+        console.log('Raw data from S3:', data); // Log the full data to inspect Contents
+        const images = data.Contents
+            .filter(item => item.Size > 0 && item.Key.match(/\.(jpeg|jpg|gif|png)$/i)) 
+            .map(item => {
+                const imageUrl = `https://{{bucket_name}}.s3.amazonaws.com/${item.Key}`; 
+                return { url: imageUrl };
             });
-            return { url };
-        });
+        console.log('Filtered images:', images); // Log the final image array to check URLs
+        return images;
+    } catch (error) {
+        console.error('Error fetching images from S3 bucket:', error);
+        return [];
+    }
+}
 
-        res.json(images);
-    });
+carrouselRouter.get('', async (req, res) => {
+    try {
+        const imageUrls = await getImageUrlsFromS3Bucket();
+        res.json(imageUrls);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 module.exports = carrouselRouter;
-
