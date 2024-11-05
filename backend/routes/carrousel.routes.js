bbe539882b616e186334c1095b56fe763ef68c99
DriesMelottePXL <75361303+DriesMelottePXL@users.noreply.github.com>
2024-11-05 20:01:58 +0100

    Update carrousel.routes.js

--- backend/routes/carrousel.routes.js
+++ backend/routes/carrousel.routes.js
@@ -1,39 +1,10 @@
 const express = require('express');
 const carrouselRouter = express.Router();
-
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
-    region: 'us-east-1',
-});
+const carrouselData = require('./../data/carrousel.json');
 
 carrouselRouter.get('', (req, res) => {
-    const params = {
-        Bucket: process.env.BUCKET,
-    };
-
-    s3.listObjectsV2(params, (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).json({ error: 'Internal Server Error' });
-        }
-
-        const jpgImages = data.Contents.filter((obj) => {
-            return obj.Key.endsWith('.jpg');
-        });
-
-        const images = jpgImages.map((obj) => {
-            const url = s3.getSignedUrl('getObject', {
-                Bucket: params.Bucket,
-                Key: obj.Key,
-                Expires: 60,
-            });
-            return { url };
-        });
-
-        res.json(images);
-    });
+    res.json(carrouselData);
 });
+
 
 module.exports = carrouselRouter;
